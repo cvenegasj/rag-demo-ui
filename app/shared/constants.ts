533a8093c801f3595331f50656a10f47ec6c5b79
Carlos Venegas <cvj_2311@hotmail.com>
2024-10-03 21:07:02 -0500

    change url beanstalk

--- app/shared/constants.ts
+++ app/shared/constants.ts
@@ -1,3 +1,3 @@
 
-// export const apiUrl = 'http://localhost:8080';
-export const apiUrl = 'http://localhost:8080';
+export const apiUrl = 'http://easy-rag-last-env.eba-fjwtmq2y.us-east-2.elasticbeanstalk.com';
+//export const apiUrl = 'http://localhost:8080';
